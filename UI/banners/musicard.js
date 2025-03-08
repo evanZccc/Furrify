a527420496761ee9958c54c4e5e5edc4a03446ad
evanZccc <167677714+evanZccc@users.noreply.github.com>
2025-03-08 18:41:07 +0100

    Add files via upload

---
+++ UI/banners/musicard.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const data = {
+    backgroundImages: [
+        path.resolve(__dirname, '../musicimages/1.png'),
+        path.resolve(__dirname, '../musicimages/2.jpg'),
+        path.resolve(__dirname, '../musicimages/3.jpg'),
+    ],
+};
+
+module.exports = data;
